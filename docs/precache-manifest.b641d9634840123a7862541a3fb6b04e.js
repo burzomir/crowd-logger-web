e2c0f56ae0e76561a6bdc1ce9528ce286c3d5ddc
burzomir <burzomirdev@gmail.com>
2020-03-21 18:44:32 +0100

    Deploy

---
+++ docs/precache-manifest.b641d9634840123a7862541a3fb6b04e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb020332d759d9addcd05122b0265ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80ae1e2108ce0b880e97",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80a788087a01465f381a",
+    "url": "/static/js/2.df9941eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df9941eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80ae1e2108ce0b880e97",
+    "url": "/static/js/main.41ea19dc.chunk.js"
+  },
+  {
+    "revision": "f7fed185375941f11557",
+    "url": "/static/js/runtime-main.00f0acf5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
